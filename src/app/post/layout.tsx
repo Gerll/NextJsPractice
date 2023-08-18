6c6e25ab3b037b509823ce914ffdf5d060ee7731
gerll <gerll.bm@hotmail.com>
2023-08-17 22:18:54 -0700

    Created a new counter component and added to the layout

--- src/app/post/layout.tsx
+++ src/app/post/layout.tsx
@@ -1,6 +1,8 @@
+import { Counter } from '../components/Counter'
 export default function PostsLayout ({ children }) {
     return (
         <div className="flex flex-col items-center justify-center">
+            <Counter/>
             <h1>Este es el layout de los post</h1>
             {children}
         </div>
